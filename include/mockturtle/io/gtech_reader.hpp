807c45c99dfbbdd069917942d128ec38248cbcf1
nlwmode <nlwmode@gmail.com>
2024-06-27 07:23:16 +0000

    adopt to the sequential network

--- include/mockturtle/io/gtech_reader.hpp
+++ include/mockturtle/io/gtech_reader.hpp
@@ -100,7 +100,7 @@ public:
     {
       if ( size.empty() )
       {
-        signals_[name] = ntk_.create_pi( name );
+        signals_[name] = ntk_.create_pi();
         input_names_.emplace_back( name, 1u );
         port_infors_.input_names.emplace_back( name, 1u );
         if constexpr ( has_set_name_v<Ntk> )
@@ -115,7 +115,7 @@ public:
         for ( auto i = 0u; i < length; ++i )
         {
           const auto sname = fmt::format( "{}[{}]", name, i );
-          word.push_back( ntk_.create_pi( sname ) );
+          word.push_back( ntk_.create_pi() );
           signals_[sname] = word.back();
           if constexpr ( has_set_name_v<Ntk> )
           {
@@ -357,7 +357,7 @@ public:
 
     for ( auto const& o : outputs_ )
     {
-      ntk_.create_po( signals_[o], o );
+      ntk_.create_po( signals_[o] );
     }
 
     if constexpr ( has_set_output_name_v<Ntk> )
